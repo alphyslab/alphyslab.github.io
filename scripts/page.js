5c6ad0f47312e373520da05f544404e57d5d41d8
DiamondLooter <dadiamondlooter@gmail.com>
2016-06-14 19:56:16 -0700

    Update page.js

--- scripts/page.js
+++ scripts/page.js
@@ -17,6 +17,6 @@ function update(element){
   toggle_visibility(element);
 }
 update("page" + page);
-if (document.location != "?page=" + page) {
+if (document.location !== "?page=" + page) {
   document.location = "?page=" + page;
 }
