dc3425a5219f9a76908a563ae0d630b9685c350c
DiamondLooter <dadiamondlooter@gmail.com>
2016-06-14 19:12:16 -0700

    Update page.js

--- scripts/page.js
+++ scripts/page.js
@@ -5,3 +5,9 @@ function nextPage(){
 function prevPage(){
   page--;
 }
+document.location = "?page=" + page;
+if (document.getElementById("page" + page) && document.getElementById("page" + page).style.visibility = "none"){
+  document.getElementById("page" + page).style.visibility = "visible";
+}else{
+  document.getElementById("page" + page).style.visibility = "none"
+}
